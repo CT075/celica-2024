0ffdab05bc10694ab21c5a96bb64f15bf0f87ee0
Cameron Wong <cam@camdar.io>
2024-03-20 13:01:44 -0400

    swap to skiphuffmandecompression
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- vendor/src/Rules.mk
+++ vendor/src/Rules.mk
@@ -5,5 +5,17 @@ d := $(dir)
 dir := $(d)/ExpandedModularSave
 include $(dir)/Rules.mk
 
+dir := $(d)/DangerZone
+include $(dir)/Rules.mk
+
+dir := $(d)/HpBars
+include $(dir)/Rules.mk
+
+dir := $(d)/LToggle
+include $(dir)/Rules.mk
+
+dir := $(d)/SkipHuffmanDecompression
+include $(dir)/Rules.mk
+
 d := $(dirstack_$(sp))
 sp := $(basename $(sp))
