91bafd567c1db9b2086c98e423805c2dd33aba97
Cameron Wong <cam@camdar.io>
2024-04-04 01:31:45 -0400

    data munging
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- data/stats/Rules.mk
+++ data/stats/Rules.mk
@@ -6,8 +6,9 @@ $(d)/%.stats.event: $(d)/%.toml $(STRUCT_MUNGER)
 	$(STRUCT_MUNGER) --input $< > $@
 
 $(d)/players.stats.event: $(d)/character.schema.toml
+$(d)/enemies.stats.event: $(d)/character.schema.toml
 
-EVENTS_$(d) := $(d)/main.event $(d)/players.stats.event
+EVENTS_$(d) := $(d)/main.event $(d)/players.stats.event $(d)/enemies.stats.event
 
 EVENTS := $(EVENTS) $(EVENTS_$(d))
 
