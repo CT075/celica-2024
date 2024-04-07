0b05ad80eaf63743975bc5ef90cb01d9ea968d50
Cameron Wong <cam@camdar.io>
2024-04-07 17:05:30 -0400

    bossquotes
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- data/tables/Rules.mk
+++ data/tables/Rules.mk
@@ -8,9 +8,10 @@ $(d)/%.table.event: $(d)/%.toml $(STRUCT_MUNGER)
 $(d)/players.table.event: $(d)/character.schema.toml
 $(d)/enemies.table.event: $(d)/character.schema.toml
 $(d)/weapons.table.event: $(d)/item.schema.toml
+$(d)/bossquotes.table.event: $(d)/bossquotes.schema.toml
 
 EVENTS_$(d) := $(d)/main.event $(d)/players.table.event $(d)/enemies.table.event \
-	$(d)/weapons.table.event
+	$(d)/weapons.table.event $(d)/bossquotes.table.event
 
 EVENTS := $(EVENTS) $(EVENTS_$(d))
 
