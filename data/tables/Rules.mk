c9a94270c4b81c4409e49426549ce0d057de9ac5
Cameron Wong <cam@camdar.io>
2024-04-07 16:53:36 -0400

    stats -> table
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- data/tables/Rules.mk
+++ data/tables/Rules.mk
@@ -2,15 +2,15 @@ sp := $(sp).x
 dirstack_$(sp) := $(d)
 d := $(dir)
 
-$(d)/%.stats.event: $(d)/%.toml $(STRUCT_MUNGER)
+$(d)/%.table.event: $(d)/%.toml $(STRUCT_MUNGER)
 	$(STRUCT_MUNGER) --input $< > $@
 
-$(d)/players.stats.event: $(d)/character.schema.toml
-$(d)/enemies.stats.event: $(d)/character.schema.toml
-$(d)/weapons.stats.event: $(d)/item.schema.toml
+$(d)/players.table.event: $(d)/character.schema.toml
+$(d)/enemies.table.event: $(d)/character.schema.toml
+$(d)/weapons.table.event: $(d)/item.schema.toml
 
-EVENTS_$(d) := $(d)/main.event $(d)/players.stats.event $(d)/enemies.stats.event \
-	$(d)/weapons.stats.event
+EVENTS_$(d) := $(d)/main.event $(d)/players.table.event $(d)/enemies.table.event \
+	$(d)/weapons.table.event
 
 EVENTS := $(EVENTS) $(EVENTS_$(d))
 
