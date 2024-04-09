4aafe79dd077d3ec0fc8060e6ef54ad064358e07
Cameron Wong <cam@camdar.io>
2024-04-09 02:31:52 -0400

    some fixes
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- data/mugs/Rules.mk
+++ data/mugs/Rules.mk
@@ -2,9 +2,10 @@ sp := $(sp).x
 dirstack_$(sp) := $(d)
 d := $(dir)
 
+MUGS_$(d) = $(shell find $(d) -type f -name '*.png')
 
-$(d)/Installer.event: $(wildcard $(d)/*.png)
-	python portaits2dmp.py
+$(d)/Installer.event: $(d)/portraits2dmp.py $(MUGS_$(d))
+	cd $(shell dirname $<) && python $(shell basename $<)
 
 EVENTS_$(d) := $(d)/main.event $(d)/Installer.event
 
