971491daf6455b0ac962e34373234e2484b825dd
Cameron Wong <cam@camdar.io>
2024-04-14 09:13:42 -0400

    fix boss deathquote, remove debugging
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- data/tables/Rules.mk
+++ data/tables/Rules.mk
@@ -9,9 +9,10 @@ $(d)/players.table.event: $(d)/character.schema.toml
 $(d)/enemies.table.event: $(d)/character.schema.toml
 $(d)/weapons.table.event: $(d)/item.schema.toml
 $(d)/bossquotes.table.event: $(d)/bossquotes.schema.toml
+$(d)/deathquotes.table.event: $(d)/deathquotes.schema.toml
 
 EVENTS_$(d) := $(d)/main.event $(d)/players.table.event $(d)/enemies.table.event \
-	$(d)/weapons.table.event $(d)/bossquotes.table.event
+	$(d)/weapons.table.event $(d)/bossquotes.table.event $(d)/deathquotes.table.event
 
 EVENTS := $(EVENTS) $(EVENTS_$(d))
 
